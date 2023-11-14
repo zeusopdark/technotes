4119c2edfcab8349fd4a5c01f8bebc9e2321fc7b
zeusopdark <ankitnishad400@gmail.com>
2023-11-14 18:15:47 +0530

    Changes

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-api.vercel.app/',
+    baseUrl: 'https://technotes-api.vercel.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
